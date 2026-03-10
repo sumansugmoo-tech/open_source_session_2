bcae4cd14f01cb74f546bf5e9fda5544b0ee66d8
sumansugmoo-tech <sumansugmoo@gmail.com>
2026-03-10 20:30:30 +0530

    Update duplicate_logic.c

--- duplicate_logic.c
+++ duplicate_logic.c
@@ -1,31 +1,9 @@
-/*
-Hint for students
-
-The program has duplicate logic in two functions.
-Refactor the code to avoid repetition.
-*/
 #include <stdio.h>
 
-int findMax(int arr[], int n)
+void findMinMax(int arr[], int n)
 {
     int i;
-    int max = arr[0];
-
-    for(i = 1; i < n; i++)
-    {
-        if(arr[i] > max)
-        {
-            max = arr[i];
-        }
-    }
-
-    return max;
-}
-
-int findMin(int arr[], int n)
-{
-    int i;
-    int min = arr[0];
+    int min = arr[0],max=min;
 
     for(i = 1; i < n; i++)
     {
@@ -33,17 +11,22 @@ int findMin(int arr[], int n)
         {
             min = arr[i];
         }
+         if(arr[i] > max)
+        {
+            max = arr[i];
+        }
+        
     }
 
-    return min;
+    printf("Max: %d\n", max);
+    printf("Min: %d\n", min);
+    return;
 }
 
-int main()
+int main(void)
 {
     int arr[5] = {60, 70, 80, 90, 50};
-
-    printf("Max: %d\n", findMax(arr, 5));
-    printf("Min: %d\n", findMin(arr, 5));
+    findMinMax(arr, 5);
 
     return 0;
 }
