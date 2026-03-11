d328186a65558726bb1d0554f1752f2969841658
sumansugmoo-tech <sumansugmoo@gmail.com>
2026-03-11 16:56:23 +0530

    Update naming_issue.c

--- naming_issue.c
+++ naming_issue.c
@@ -1,42 +1,41 @@
 #include <stdio.h>
 
-int x[100];
-int y = 0;
-
-int f1()
+int calculateAverage(int studentMarks[], int studentCount)
 {
     int i;
-    int s = 0;
+    int sum = 0;
 
-    for(i = 0; i < y; i++)
-    {
-        s = s + x[i];
-    }
-
-    if(y == 0)
+    if(studentCount == 0)
     {
         return 0;
     }
 
-    return s / y;
+    for(i = 0; i < studentCount; i++)
+    {
+        sum = sum + studentMarks[i];
+    }
+
+    return sum / studentCount;
 }
 
 int main()
 {
+    int marks[100];
+    int studentCount = 0;
     int i;
 
     printf("Enter number of students: ");
-    scanf("%d", &y);
+    scanf("%d", &studentCount);
 
-    for(i = 0; i < y; i++)
+    for(i = 0; i < studentCount; i++)
     {
         printf("Enter marks: ");
-        scanf("%d", &x[i]);
+        scanf("%d", &marks[i]);
     }
 
-    int avg = f1();
+    int average = calculateAverage(marks, studentCount);
 
-    printf("Average marks: %d\n", avg);
+    printf("Average marks: %d\n", average);
 
     return 0;
 }
